c4c67189a0db75f5fa6207e5dbcae12abc664d0b
Maxim Shabanov <99maxwolf99@gmail.com>
2024-02-27 21:24:03 +0300

    Add files via upload

---
+++ src/main/java/com/example/JVTSecure/user/Role.java
@@ -0,0 +1,7 @@
+package com.example.JVTSecure.user;
+
+public enum Role {
+    ADMIN,
+    USER,
+    MANAGER
+}
---
+++ src/test/java/com/example/JVTSecure/JvtSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JVTSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvtSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
